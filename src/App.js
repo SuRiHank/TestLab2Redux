43228cc4706a25a3bd1714026abd1c104b367c61
Hank <dangphuochan2003@gmail.com>
2023-10-21 16:38:29 +0700

    Lab2 Template

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent.js";
+import Hello from "./components/class/Hello.js";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Hello myId={"DE170429"} myAge="23" a={10} b={5} />
+//       <Menu dishes={DISHES} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import GreetingContainer from "./components/class/GreetingContainer.js";
+import RandomUser from "./components/class/RandomUser.js";
+import Main from "./components/MainComponent.js";
+import { BrowserRouter } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} /> */}
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
